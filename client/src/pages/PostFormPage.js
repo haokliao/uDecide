15849ba9def894903016888420efefe3f2a2afbe
Kev <haokliao@gmail.com>
2021-12-08 15:07:02 -0500

    fixing styling for create post page

--- client/src/pages/PostFormPage.js
+++ client/src/pages/PostFormPage.js
@@ -150,9 +150,8 @@ class PostFormPage extends React.Component {
     return (
       <div className="container-fluid">
         {/*Components and styles imported from src/Components/PostComponents */}
-        {/* <div className="row"> */}
-          {errorMessage}
-          {/* <div className="input-group">
+
+        {/* <div className="input-group">
             <input
               type="text"
               placeholder="Add your words of wisdom here..."
@@ -165,153 +164,160 @@ class PostFormPage extends React.Component {
             </button>
           </div> */}
 
-          <div className="row d-flex flex-row">
-            {/*<!-- Left side Content Area -->*/}
-            <div className="col-sm-5 leftColumn"> 
-            <div className="row tabArea mb-3">
-            {/*<!-- <h6 class="createPostTab"><span class="createPostTabTextStyling">Create a Post</span></h6> -->*/}
-            <h5 className="badge createPostTab">Create a Post</h5>
-          </div>
-              <div method="get" id="postSubmission">
-                {/* <PostTitle /> */}
+        {errorMessage}
+
+        {/* <div className="row d-flex flex-row">
+        <div className="col-sm-5 leftColumn"> */}
+
+        {/*<!-- Left side Content Area -->*/}
+        <div className="row tabArea mb-3">
+          {/*<!-- <h6 class="createPostTab"><span class="createPostTabTextStyling">Create a Post</span></h6> -->*/}
+          <h5 className="badge createPostTab">Create a Post</h5>
+        </div>
+
+        <div class="row gx-2 d-flex flex-row">
+          <div class="col-sm-6 col-lg-5 leftColumn">
+
+            <div method="get" id="postSubmission">
+              {/* <PostTitle /> */}
+              <div className="form-group">
+                <label htmlFor="postTitle">
+                  <h6 className="labelFontSize">Title</h6>
+                </label>
+                <input
+                  type="text"
+                  className="form-control"
+                  id="postTitle"
+                  placeholder="What's on your mind?"
+                  value={this.state.title}
+                  onChange={this.titleChanged}
+                />
+              </div>
+              {/* <PostDescription /> */}
+              <div className="form-group">
+                <label htmlFor="postDescription">
+                  <h6 className="labelFontSize">Description</h6>
+                </label>
+                <textarea
+                  className="form-control"
+                  id="postDescription"
+                  rows="2"
+                  placeholder="What's been on your mind?"
+                  value={this.state.content}
+                  onChange={this.contentChanged}
+                ></textarea>
+              </div>
+              {/* <OptionComponents /> */}
+              <div>
                 <div className="form-group">
-                  <label htmlFor="postTitle">
-                    <h6 className="labelFontSize">Title</h6>
-                  </label>
-                  <input
-                    type="text"
-                    className="form-control"
-                    id="postTitle"
-                    placeholder="What's on your mind?"
-                    value={this.state.title}
-                    onChange={this.titleChanged}
-                  />
-                </div>
-                {/* <PostDescription /> */}
-                <div className="form-group">
-                  <label htmlFor="postDescription">
-                    <h6 className="labelFontSize">Description</h6>
+                  <label htmlFor="option1">
+                    <h6 className="labelFontSize">Option 1</h6>
                   </label>
                   <textarea
                     className="form-control"
-                    id="postDescription"
+                    id="option1"
                     rows="2"
-                    placeholder="What's been on your mind?"
-                    value={this.state.content}
-                    onChange={this.contentChanged}
+                    placeholder="What's the first thing you need help choosing between?"
+                    value={this.state.optOne}
+                    onChange={this.optOneChanged}
                   ></textarea>
                 </div>
-                {/* <OptionComponents /> */}
-                <div>
-                  <div className="form-group">
-                    <label htmlFor="option1">
-                      <h6 className="labelFontSize">Option 1</h6>
-                    </label>
-                    <textarea
-                      className="form-control"
-                      id="option1"
-                      rows="2"
-                      placeholder="What's the first thing you need help choosing between?"
-                      value={this.state.optOne}
-                      onChange={this.optOneChanged}
-                    ></textarea>
-                  </div>
-                  <div className="form-group">
-                    <label htmlFor="postDescription">
-                      <h6 className="labelFontSize">Option 2</h6>
-                    </label>
-                    <textarea
-                      className="form-control"
-                      id="option2"
-                      rows="2"
-                      placeholder="What's the second thing you need help choosing between?"
-                      value={this.state.optTwo}
-                      onChange={this.optTwoChanged}
-                    ></textarea>
-                  </div>
-                  <div className="form-group">
-                    <label htmlFor="postDescription">
-                      <h6 className="labelFontSize">Option 3</h6>
-                    </label>
-                    <textarea
-                      className="form-control"
-                      id="option3"
-                      rows="2"
-                      placeholder="What's the third thing you need help choosing between?"
-                      value={this.state.optThree}
-                      onChange={this.optThreeChanged}
-                    ></textarea>
-                  </div>
+                <div className="form-group">
+                  <label htmlFor="postDescription">
+                    <h6 className="labelFontSize">Option 2</h6>
+                  </label>
+                  <textarea
+                    className="form-control"
+                    id="option2"
+                    rows="2"
+                    placeholder="What's the second thing you need help choosing between?"
+                    value={this.state.optTwo}
+                    onChange={this.optTwoChanged}
+                  ></textarea>
                 </div>
-                {/* <PostSubmitButton /> */}
-                <div>
-                  <input
-                    type="submit"
-                    className="btn w-100 mt-4 mb-4"
-                    value="Submit Post"
-                    onClick={this.savePost}
-                  />
+                <div className="form-group">
+                  <label htmlFor="postDescription">
+                    <h6 className="labelFontSize">Option 3</h6>
+                  </label>
+                  <textarea
+                    className="form-control"
+                    id="option3"
+                    rows="2"
+                    placeholder="What's the third thing you need help choosing between?"
+                    value={this.state.optThree}
+                    onChange={this.optThreeChanged}
+                  ></textarea>
                 </div>
               </div>
+              {/* <PostSubmitButton /> */}
+              <div>
+                <input
+                  type="submit"
+                  className="btn w-100 mt-4 mb-4"
+                  value="Submit Post"
+                  onClick={this.savePost}
+                />
+              </div>
             </div>
+          </div>
 
 
-             {/* <!-- Right side content area --> */}
-            <div className="col-sm-6 offset-1 rightColumn">
-              <article className="userVotingArea row g-0 font-weight-bold">
-                <h5 className="titleArea text-center labelFontSize">=Title</h5>
-                <div className="d-flex flex-row justify-content-between px-3">
-                  <div className="row flexItem1 mt-1 me-2">
-                    <i
-                      className="
+          {/* <!-- Right side content area --> */}
+          <div className="col-sm-6 col-lg-6 offset-1 rightColumn">
+            <article className="userVotingArea row g-0 font-weight-bold">
+              <h5 className="titleArea text-center labelFontSize">=Title</h5>
+              <div className="d-flex flex-row justify-content-between px-3">
+                <div className="row flexItem1 mt-1 me-2">
+                  <i
+                    className="
                     publicUserPic
                     col-sm-3
                     bi bi-person-circle
                     iconSize
                     mt-1
                   "
-                    ></i>
-                    <div className="profileName col-sm-9">
-                      <p className="m-0">natureisrad55</p>
-                      <p className="m-0">3 hrs ago</p>
-                    </div>
-                  </div>
-                  <div className="flexItem2">
-                    <div className="profileVotes">
-                      <p className="m-1">Total Votes:</p>
-                      <p className="m-1 text-center">800</p>
-                    </div>
+                  ></i>
+                  <div className="profileName col-sm-9">
+                    <p className="m-0">natureisrad55</p>
+                    <p className="m-0">3 hrs ago</p>
                   </div>
                 </div>
-                <p className="row question contentPadding mt-3">
-                  Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-                  Eligendi dolorem maiores nulla, assumenda beatae similique a
-                  voluptatibus.
-                </p>
-              </article>
-
-
-              <div>
-                <article className="votingOptions row g-0">
-                  <h5 className="votingInProgressComponent mt-2 text-center">
-                    Voting In Progress
-                  </h5>
-                  <div className="">
-                    <h6 className="option p-2 mb-2">=Option 1</h6>
+                <div className="flexItem2">
+                  <div className="profileVotes">
+                    <p className="m-1">Total Votes:</p>
+                    <p className="m-1 text-center">800</p>
                   </div>
-                  <div className="">
-                    <h6 className="option p-2">=Option 2</h6>
-                  </div>
-                  <div className="">
-                    <h6 className="option p-2">=Option 3</h6>
-                  </div>
-                </article>
+                </div>
               </div>
-            </div> 
+              <p className="row question contentPadding mt-3">
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                Eligendi dolorem maiores nulla, assumenda beatae similique a
+                voluptatibus.
+              </p>
+            </article>
 
 
+            <div>
+              <article className="votingOptions row g-0">
+                <h5 className="votingInProgressComponent mt-2 text-center">
+                  Voting In Progress
+                </h5>
+                <div className="">
+                  <h6 className="option p-2 mb-2">=Option 1</h6>
+                </div>
+                <div className="">
+                  <h6 className="option p-2">=Option 2</h6>
+                </div>
+                <div className="">
+                  <h6 className="option p-2">=Option 3</h6>
+                </div>
+              </article>
+            </div>
           </div>
+
+
         </div>
+      </div>
     );
   }
 }
